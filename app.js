f4a3880a56aefaef4f24573b657ece9d7cd40835
unknown <ankur28november@gmail.com>
2020-09-02 00:46:14 -0700

    cg

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const httpServer = require("http").createServer(app);
 const socketServer = require("socket.io")(httpServer);
 // const path = require("path");
 app.use(express.static("activity"));
+app.get('/home', function (req, res) {
+    console.log('home');
+})
 socketServer.on("connection", function (socket) {
     console.log("New client connected")
     console.log(socket.id);
@@ -13,6 +16,10 @@ socketServer.on("connection", function (socket) {
     socket.on("colorChange", function (color) {
         console.log(color);
         socket.broadcast.emit('rColorChange', color);
+    })
+    socket.on('widthChange', function( width ){
+        console.log('width change :'+width );
+        socket.broadcast.emit( 'serverWidthChange', width );
     })
     socket.on("md", function (point) {
         console.log(point);
@@ -25,6 +32,6 @@ socketServer.on("connection", function (socket) {
 })
 //  tcp => uniquely identify server on a machine
 let port = process.env.PORT || 3000;
-httpServer.listen(port, function () {
+httpServer.listen(port, function (req,res) {
     console.log("Server is listening to request at port 3000");
 })
